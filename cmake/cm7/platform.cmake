c70cabe7a3e3d89ce5431eac0dc84773e051bc65
Jacob <jacob.heathorn@gmail.com>
2024-04-29 10:25:45 +0700

    Automatically find core by build type

--- cmake/cm7/platform.cmake
+++ cmake/cm7/platform.cmake
@@ -113,33 +113,51 @@ endfunction()
 # Other utilities
 
 # TODO move to forge
-# Function to find a file recursively in a given directory
+# Findsy a file recursively in a given directory
 function(FIND_FILE_IN_DIRECTORY result_var input_directory input_filename)
-    # Use GLOB_RECURSE to search for the file recursively
-    file(GLOB_RECURSE found_files
-        RELATIVE "${input_directory}"
-        "${input_directory}/${input_filename}")
+  # Use GLOB_RECURSE to search for the file recursively
+  file(GLOB_RECURSE found_files
+    RELATIVE "${input_directory}"
+    "${input_directory}/${input_filename}")
 
-    # Check the number of files found
-    list(LENGTH found_files num_files)
-    if(num_files EQUAL 1)
-        list(GET found_files 0 first_file_path)
-        # Construct the full path
-        set(full_path "${input_directory}/${first_file_path}")
-        set("${result_var}" "${full_path}" PARENT_SCOPE)
-        message(STATUS "File found: ${full_path}")
-    elseif(num_files GREATER 1)
-        message(FATAL_ERROR "Error: Multiple instances of '${input_filename}' found in directory '${input_directory}'.")
-    else()
-        message(FATAL_ERROR "Error: The file '${input_filename}' not found in directory '${input_directory}'")
-    endif()
+  # Check the number of files found
+  list(LENGTH found_files num_files)
+  if(num_files EQUAL 1)
+    list(GET found_files 0 first_file_path)
+    # Construct the full path
+    set(full_path "${input_directory}/${first_file_path}")
+    set("${result_var}" "${full_path}" PARENT_SCOPE)
+    message(STATUS "File found: ${full_path}")
+  elseif(num_files GREATER 1)
+    message(FATAL_ERROR "Error: Multiple instances of '${input_filename}' found in directory '${input_directory}'.")
+  else()
+    message(FATAL_ERROR "Error: The file '${input_filename}' not found in directory '${input_directory}'")
+  endif()
 endfunction()
 
-# Function to find the core0 debug file
+# Finds the core0 debug binary source
 function(FIND_CORE0_DEBUG result_var input_filename)
-    FIND_FILE_IN_DIRECTORY(FILE_PATH "$ENV{PROJECT_ROOT}/bin/cm4-debug" "${input_filename}")
+  FIND_FILE_IN_DIRECTORY(FILE_PATH "$ENV{PROJECT_ROOT}/bin/cm4-debug" "${input_filename}")
+  set(${result_var} ${FILE_PATH} PARENT_SCOPE)
+  message(STATUS "Core0 found: ${FILE_PATH}")
+endfunction()
 
-    # Pass the result to the caller's variable
-    set(${result_var} ${FILE_PATH} PARENT_SCOPE)
-    message(STATUS "Found core 0 binary source: ${FILE_PATH}")
+# Finds the core0 release binary source
+function(FIND_CORE0_RELEASE result_var input_filename)
+  FIND_FILE_IN_DIRECTORY(FILE_PATH "$ENV{PROJECT_ROOT}/bin/cm4-release" "${input_filename}")
+  set(${result_var} ${FILE_PATH} PARENT_SCOPE)
+  message(STATUS "Core0 found: ${FILE_PATH}")
+endfunction()
+
+# Finds either the release or debug version of the core0 binary source depending on the local build
+# type
+function(FIND_CORE0 result_var input_filename)
+  if("${CMAKE_BUILD_TYPE}" STREQUAL "Debug")
+    FIND_CORE0_DEBUG(CORE0 "${input_filename}")
+  elseif("${CMAKE_BUILD_TYPE}" STREQUAL "Release")
+    FIND_CORE0_RELEASE(CORE0 "${input_filename}")
+  else()
+    message(FATAL_ERROR "Unsupported build type: ${CMAKE_BUILD_TYPE}")
+  endif()
+  set(${result_var} ${CORE0} PARENT_SCOPE)
 endfunction()
