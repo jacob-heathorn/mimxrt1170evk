740dc7a7f9c989158595fcd85403bba5f19b5416
Jacob <jacob.heathorn@gmail.com>
2024-05-01 08:49:11 +0700

    Cleanup

--- scripts/python/nxp/vscode_debugger.py
+++ scripts/python/nxp/vscode_debugger.py
@@ -24,6 +24,22 @@ class VSCodeDebugger():
 
     launch_manager.update(template_fullfile, context)
     return name
+  
+  def generate_core1_launch_config(self, executable: os.path):
+    launch_fullfile = os.path.join(PROJECT_ROOT, '.vscode', 'launch.json')
+    template_fullfile = os.path.join(PROJECT_ROOT, 'scripts', 'templates', 'core1_launch_config.jinja2')
+
+    launch_manager = vscode.LaunchManager(launch_fullfile)
+
+    # Define the context for your template rendering
+    name = 'core1 (cortex m4)'
+    context = {
+      'name': name,
+      'executable': executable
+    }
+
+    launch_manager.update(template_fullfile, context)
+    return name
 
   def generate_linkserver_task(self):
     tasks_fullfile = os.path.join(PROJECT_ROOT, '.vscode', 'tasks.json')
@@ -32,10 +48,7 @@ class VSCodeDebugger():
     tasks_manager = vscode.TasksManager(tasks_fullfile)
 
     # Define the context for your template rendering
-    context = {
-      'label': 'todo'
-    }
-
+    context = {}
     tasks_manager.update(template_fullfile, context)
 
   def generate_core0(self, executable: os.path):
@@ -45,16 +58,6 @@ class VSCodeDebugger():
 
 
   def generate_core1(self, executable: os.path):
-    launch_fullfile = os.path.join(PROJECT_ROOT, '.vscode', 'launch.json')
-    template_fullfile = os.path.join(PROJECT_ROOT, 'scripts', 'templates', 'core1_launch_config.jinja2')
-
-    launch_manager = vscode.LaunchManager(launch_fullfile)
-
-    # Define the context for your template rendering
-    context = {
-      'name': 'core1 (cortex m4)',
-      'executable': executable
-    }
-
-    launch_manager.update(template_fullfile, context)
-    print_green(f"In VSCode use run config: {context['name']}")
+    self.generate_linkserver_task()
+    name = self.generate_core1_launch_config(executable)
+    print_green(f"In VSCode use run config: {name}")
