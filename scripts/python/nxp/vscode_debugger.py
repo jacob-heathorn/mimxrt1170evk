ce4631a6de92d18e2ad7b1ec527d842bba5396b6
Jacob <jacob.heathorn@gmail.com>
2024-05-01 08:44:04 +0700

    tasks manager

--- scripts/python/nxp/vscode_debugger.py
+++ scripts/python/nxp/vscode_debugger.py
@@ -1,6 +1,6 @@
 import os
 from forge.helpers import print_green
-from launch_manager import LaunchManager
+import vscode
 
 PROJECT_ROOT = os.environ.get("PROJECT_ROOT")
 
@@ -9,26 +9,46 @@ class VSCodeDebugger():
   def __init__(self):
     pass
 
-  def generate_core0(self, executable: os.path):
+  def generate_core0_launch_config(self, executable: os.path):
     launch_fullfile = os.path.join(PROJECT_ROOT, '.vscode', 'launch.json')
     template_fullfile = os.path.join(PROJECT_ROOT, 'scripts', 'templates', 'core0_launch_config.jinja2')
 
-    manager = LaunchManager(launch_fullfile)
+    launch_manager = vscode.LaunchManager(launch_fullfile)
 
     # Define the context for your template rendering
+    name = 'core0 (cortex m7)'
     context = {
-      'name': 'core0 (cortex m7)',
+      'name': name,
       'executable': executable
     }
 
-    manager.update(template_fullfile, context)
-    print_green(f"In VSCode use run config: {context['name']}")
+    launch_manager.update(template_fullfile, context)
+    return name
+
+  def generate_linkserver_task(self):
+    tasks_fullfile = os.path.join(PROJECT_ROOT, '.vscode', 'tasks.json')
+    template_fullfile = os.path.join(PROJECT_ROOT, 'scripts', 'templates', 'linkserver_task.jinja2')
+
+    tasks_manager = vscode.TasksManager(tasks_fullfile)
+
+    # Define the context for your template rendering
+    context = {
+      'label': 'todo'
+    }
+
+    tasks_manager.update(template_fullfile, context)
+
+  def generate_core0(self, executable: os.path):
+    self.generate_linkserver_task()
+    name = self.generate_core0_launch_config(executable)
+    print_green(f"In VSCode use run config: {name}")
+
 
   def generate_core1(self, executable: os.path):
     launch_fullfile = os.path.join(PROJECT_ROOT, '.vscode', 'launch.json')
     template_fullfile = os.path.join(PROJECT_ROOT, 'scripts', 'templates', 'core1_launch_config.jinja2')
 
-    manager = LaunchManager(launch_fullfile)
+    launch_manager = vscode.LaunchManager(launch_fullfile)
 
     # Define the context for your template rendering
     context = {
@@ -36,5 +56,5 @@ class VSCodeDebugger():
       'executable': executable
     }
 
-    manager.update(template_fullfile, context)
+    launch_manager.update(template_fullfile, context)
     print_green(f"In VSCode use run config: {context['name']}")
---
+++ scripts/python/vscode/__init__.py
@@ -0,0 +1,2 @@
+from .launch_manager import LaunchManager
+from .tasks_manager import TasksManager
