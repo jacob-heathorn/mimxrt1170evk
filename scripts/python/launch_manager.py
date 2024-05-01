b190435e4ad91b3a9c365896a019a8269e0a2a68
Jacob <jacob.heathorn@gmail.com>
2024-05-01 08:25:52 +0700

    Format

--- scripts/python/launch_manager.py
+++ scripts/python/launch_manager.py
@@ -10,25 +10,25 @@ class LaunchManager:
     self.data = self._load_or_create_default()
 
   def _load_or_create_default(self):
-        if not os.path.exists(self.file):
-            # File does not exist, create it with default configuration
-            return self._create_default_file()
-        else:
-            # File exists, load its content
-            with open(self.file, 'r') as file:
-                try:
-                    data = json5.load(file)
-                except ValueError:
-                    # Handle case where file is empty or contains invalid JSON5
-                    return self._create_default_file()
+    if not os.path.exists(self.file):
+      # File does not exist, create it with default configuration
+      return self._create_default_file()
+    else:
+      # File exists, load its content
+      with open(self.file, 'r') as file:
+        try:
+          data = json5.load(file)
+        except ValueError:
+          # Handle case where file is empty or contains invalid JSON5
+          return self._create_default_file()
 
-                # Check if 'configurations' key exists
-                if 'configurations' not in data:
-                    # Add default configurations and save the file
-                    data['configurations'] = []
-                    with open(self.file, 'w') as file:
-                        json5.dump(data, file, indent=4)
-                return data
+        # Check if 'configurations' key exists
+        if 'configurations' not in data:
+          # Add default configurations and save the file
+          data['configurations'] = []
+          with open(self.file, 'w') as file:
+            json5.dump(data, file, indent=4)
+        return data
   
   def _create_default_file(self):
         default_data = {
