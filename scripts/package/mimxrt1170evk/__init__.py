4a68d1501fffc784243f0924c26eba1c8ac245ae
Jacob <jacob.heathorn@gmail.com>
2025-01-17 21:36:34 -0500

    WIP forge.application
    * Bump forge to 6435ccc4ec4d1a87a914974dea575591e2d896b8

--- scripts/package/mimxrt1170evk/__init__.py
+++ scripts/package/mimxrt1170evk/__init__.py
@@ -1,5 +1,4 @@
 # flake8: noqa: F401
 from .vscode_debugger import VSCodeDebugger
 from .core0_flasher import Core0Flasher
-from .bin import resolve_application
 from .commands import flash_core_0, debug_core_0, debug_core_1
--- scripts/package/mimxrt1170evk/commands.py
+++ scripts/package/mimxrt1170evk/commands.py
@@ -1,4 +1,5 @@
 import mimxrt1170evk
+import forge
 from forge.helpers import error
 
 
@@ -7,7 +8,7 @@ def flash_core_0(preset_application: str):
   Expects a string in the format preset:application, so that the executable can be found in the bin
   directory and flashed to hardware. The preset must be a cm7 variant (e.g. cm7-debug).
   """
-  preset, application = mimxrt1170evk.bin.resolve_application(preset_application)
+  preset, application = forge.bin.resolve_application(preset_application)
 
   if preset.startswith("cm7"):
     flasher = mimxrt1170evk.Core0Flasher()
@@ -22,7 +23,7 @@ def debug_core_0(preset_application: str):
   directory and used to setup the debug environment. The preset must be a cm7 variant (e.g.
   cm7-debug).
   """
-  preset, application = mimxrt1170evk.bin.resolve_application(preset_application)
+  preset, application = forge.bin.resolve_application(preset_application)
 
   if preset.startswith("cm7"):
     debugger = mimxrt1170evk.VSCodeDebugger()
@@ -37,7 +38,7 @@ def debug_core_1(preset_application: str):
   directory and used to setup the debug environment. The preset must be a cm4 variant (e.g.
   cm4-debug).
   """
-  preset, application = mimxrt1170evk.bin.resolve_application(preset_application)
+  preset, application = forge.bin.resolve_application(preset_application)
 
   if preset.startswith("cm4"):
     debugger = mimxrt1170evk.VSCodeDebugger()
