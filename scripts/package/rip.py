4010a1df7d6620f8ec51eb10200d6f99d2085f34
Jacob <jacob.heathorn@gmail.com>
2025-02-22 10:56:52 -0500

    Cleanup generator
    * Bump forge to 1478b26356e9cc0332c240a21899e7a7a2f30423

--- scripts/package/rip.py
+++ scripts/package/rip.py
@@ -44,10 +44,6 @@ def main():
     output_dir = os.path.join(PROJECT_ROOT, 'firmware', 'cm4', 'registers')
     svd_parser_wrapper = forge.SVDParserWrapper(file, output_dir)
     svd_parser_wrapper.generate()
-    # svd_parser_wrapper.generate_peripheral("LPUART1")
-    # svd_parser_wrapper.generate_peripheral('GPIO9')
-    # svd_parser_wrapper.generate_peripheral('IOMUXC_GPR')
-    # svd_parser_wrapper.generate_peripheral('IOMUXC')
 
 
 if __name__ == '__main__':
