3be08213ae8d538c308470eef905b60050ac4a0b
Jacob <jacob.heathorn@gmail.com>
2025-02-26 09:21:17 -0500

    Cleanup

--- firmware/cm4/drivers_new/gpio.hpp
+++ firmware/cm4/drivers_new/gpio.hpp
@@ -53,55 +53,81 @@ struct Gpio;
 
 template <> struct Gpio<1> {
   using DR = nGPIO1::DR; 
-  using DR_TOGGLE = nGPIO1::DR_TOGGLE; 
+  using DR_TOGGLE = nGPIO1::DR_TOGGLE;
+  using GDIR = nGPIO1::GDIR;
+  using IMR = nGPIO1::IMR;
 };
 template <> struct Gpio<2> {
   using DR = nGPIO2::DR; 
-  using DR_TOGGLE = nGPIO2::DR_TOGGLE; 
+  using DR_TOGGLE = nGPIO2::DR_TOGGLE;
+  using GDIR = nGPIO2::GDIR;
+  using IMR = nGPIO2::IMR;
 };
 template <> struct Gpio<3> {
   using DR = nGPIO3::DR; 
-  using DR_TOGGLE = nGPIO3::DR_TOGGLE; 
+  using DR_TOGGLE = nGPIO3::DR_TOGGLE;
+  using GDIR = nGPIO3::GDIR;
+  using IMR = nGPIO3::IMR;
 };
 template <> struct Gpio<4> {
   using DR = nGPIO4::DR; 
-  using DR_TOGGLE = nGPIO4::DR_TOGGLE; 
+  using DR_TOGGLE = nGPIO4::DR_TOGGLE;
+  using GDIR = nGPIO4::GDIR;
+  using IMR = nGPIO4::IMR;
 };
 template <> struct Gpio<5> {
   using DR = nGPIO5::DR; 
-  using DR_TOGGLE = nGPIO5::DR_TOGGLE; 
+  using DR_TOGGLE = nGPIO5::DR_TOGGLE;
+  using GDIR = nGPIO5::GDIR;
+  using IMR = nGPIO5::IMR;
 };
 template <> struct Gpio<6> {
   using DR = nGPIO6::DR; 
   using DR_TOGGLE = nGPIO6::DR_TOGGLE; 
+  using GDIR = nGPIO6::GDIR;
+  using IMR = nGPIO6::IMR;
 };
 template <> struct Gpio<7> {
   using DR = nGPIO7::DR; 
-  using DR_TOGGLE = nGPIO7::DR_TOGGLE; 
+  using DR_TOGGLE = nGPIO7::DR_TOGGLE;
+  using GDIR = nGPIO7::GDIR;
+  using IMR = nGPIO7::IMR;
 };
 template <> struct Gpio<8> {
   using DR = nGPIO8::DR; 
-  using DR_TOGGLE = nGPIO8::DR_TOGGLE; 
+  using DR_TOGGLE = nGPIO8::DR_TOGGLE;
+  using GDIR = nGPIO8::GDIR;
+  using IMR = nGPIO8::IMR;
 };
 template <> struct Gpio<9> {
   using DR = nGPIO9::DR; 
   using DR_TOGGLE = nGPIO9::DR_TOGGLE;
+  using GDIR = nGPIO9::GDIR;
+  using IMR = nGPIO9::IMR;
 };
 template <> struct Gpio<10> {
   using DR = nGPIO10::DR; 
-  using DR_TOGGLE = nGPIO10::DR_TOGGLE; 
+  using DR_TOGGLE = nGPIO10::DR_TOGGLE;
+  using GDIR = nGPIO10::GDIR;
+  using IMR = nGPIO10::IMR;
 };
 template <> struct Gpio<11> {
   using DR = nGPIO11::DR; 
-  using DR_TOGGLE = nGPIO11::DR_TOGGLE; 
+  using DR_TOGGLE = nGPIO11::DR_TOGGLE;
+  using GDIR = nGPIO11::GDIR;
+  using IMR = nGPIO11::IMR;
 };
 template <> struct Gpio<12> {
   using DR = nGPIO12::DR; 
-  using DR_TOGGLE = nGPIO12::DR_TOGGLE; 
+  using DR_TOGGLE = nGPIO12::DR_TOGGLE;
+  using GDIR = nGPIO12::GDIR;
+  using IMR = nGPIO12::IMR;
 };
 template <> struct Gpio<13> {
   using DR = nGPIO13::DR; 
   using DR_TOGGLE = nGPIO13::DR_TOGGLE;
+  using GDIR = nGPIO13::GDIR;
+  using IMR = nGPIO13::IMR;
 };
 }
 
@@ -126,28 +152,24 @@ void Gpio<GPIO_NUM>::configure(GpioDirection dir, GpioPull pull) {
   EnableGpioClock();
   configurePinMux();
 
-  // Configure direction
+  // Configure direction.
   if (dir == GpioDirection::eOutput) {
-    // TODO templatize?
-    nGPIO9::IMR::Instance().bits.IMR &= ~(1UL << pin_);
-    nGPIO9::DR::Instance().bits.DR &= ~(1UL << pin_);
-    nGPIO9::GDIR::Instance().bits.GDIR |= (1UL << pin_);
+    Registers::Gpio<GPIO_NUM>::IMR::Instance().bits.IMR &= ~(1UL << pin_);
+    Registers::Gpio<GPIO_NUM>::DR::Instance().bits.DR &= ~(1UL << pin_);
+    Registers::Gpio<GPIO_NUM>::GDIR::Instance().bits.GDIR |= (1UL << pin_);
   } else {
-      // Set pin as input
-      // GPIOx->GDIR &= ~(1 << pin_);
+    assert(false);  // TODO: implement eInput
   }
 
   // Configure pull-up/down resistors
   if (pull == GpioPull::ePullUp) {
-      // Enable pull-up resistor
-      // GPIOx->PULL |= (1 << pin_);
+    assert(false);  // TODO: implement pull up.
   } else if (pull == GpioPull::ePullDown) {
-      // Enable pull-down resistor
-      // GPIOx->PULL &= ~(1 << pin_);
+    assert(false);  // TODO: implement pull down.
   }
 }
 
-// Write HIGH or LOW to the GPIO pin
+// Write state to the GPIO pin.
 template <uint32_t GPIO_NUM>
 void Gpio<GPIO_NUM>::write(bool state) {
     if (state == true) {
@@ -157,18 +179,16 @@ void Gpio<GPIO_NUM>::write(bool state) {
     }
 }
 
-// Read the current state of the GPIO pin
+// Read the current state of the GPIO pin.
 template <uint32_t GPIO_NUM>
 bool Gpio<GPIO_NUM>::read() {
-    // Example: return (GPIOx->DR & (1 << pin_)) ? HIGH : LOW;
-    return false;  // Placeholder
+    assert(false);  // TODO: implement read.
+    return false;
 }
 
-// Toggle the state of the GPIO pin
+// Toggle the state of the GPIO pin.
 template <uint32_t GPIO_NUM>
 void Gpio<GPIO_NUM>::toggle() {
-    // Example: GPIOx->DR ^= (1 << pin_);
-    // nGPIO9::DR_TOGGLE::Instance().bits.DR_TOGGLE ^= (1 << pin_);
     Registers::Gpio<GPIO_NUM>::DR_TOGGLE::Instance().bits.DR_TOGGLE ^= (1 << pin_);
 }
 
@@ -192,6 +212,6 @@ void Gpio<9>::configurePinMux() {
           nIOMUXC::SW_MUX_CTL_PAD_GPIO_AD_04::eMUX_MODE::eALT10_gpio9_IO3;
         break;
       default:
-        assert(false);
+        assert(false);  // TODO: pin not implemented yet.
     }
 }
