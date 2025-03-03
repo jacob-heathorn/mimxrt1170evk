b32673e7e21e1074fc485cea324315add8130180
Jacob <jacob.heathorn@gmail.com>
2025-03-03 07:05:41 -0500

    ctctest.py

---
+++ scripts/package/mimxrt1170evk/ctest.py
@@ -0,0 +1,44 @@
+import serial
+import os
+import re
+import sys
+import mimxrt1170evk
+
+# Pull in environment variables
+SERIAL_DEVICE = os.environ.get("SERIAL_DEVICE")
+
+
+def do(test_application: mimxrt1170evk.Core0Application):
+
+  # Initialize serial port.
+  ser = serial.Serial(SERIAL_DEVICE, 115200)
+
+  # Flash the test application.
+  test_application.flash()
+
+  # Analyze test output
+  suite_passed = True
+  while True:
+    # Read next line form UART output
+    line = ser.readline().decode('utf-8').strip('\r\n')
+
+    # Print the line so it is available in ctest output.
+    print(line)
+
+    # Check if any test case failed
+    if "\033[31mFAILED\033[0m" in line:
+      suite_passed = False
+
+    # Use regex to check for the summary line to determine if the test suite has completed. TODO:
+    # The summary line always prints PASSED with the number of passed tests (even if it's 0). I feel
+    # it should print FAILED if any test case failed, then we can just look at that summary line to
+    # determine overall pass/fail. For now, we are looking for FAILED in any of the lines to
+    # determine overall suite failure and return status.
+    pattern = r"^\[\s+(\033\[32mPASSED\033\[0m|\033\[31mFAILED\033\[0m)\s+\]\s+(\d+)\s+test\(s\)\.$"
+
+    match = re.match(pattern, line)
+    if match:
+      if suite_passed:
+        sys.exit(0)
+      else:
+        sys.exit(1)
--- scripts/package/pw_ctest.py
+++ scripts/package/pw_ctest.py
@@ -4,14 +4,7 @@
 
 # System pythonmodules
 import argparse
-import serial
-import os
-import re
-import sys
 import mimxrt1170evk
-
-# Pull in environment variables
-SERIAL_DEVICE = os.environ.get("SERIAL_DEVICE")
 
 
 def main():
@@ -21,41 +14,8 @@ def main():
   #                     help='Pigweed unit test runnable to run on the target')
   args = parser.parse_args()
 
-  # Initialize serial port
-  ser = serial.Serial(SERIAL_DEVICE, 115200)
-
-  # Flash the test program
-  if args.flash_core_0:
-    mimxrt1170evk.Core0Application(args.flash_core_0).flash()
-  # args = ['rip', '-r', args.test]
-  # subprocess.check_call(args)
-
-  # Analyze test output
-  suite_passed = True
-  while True:
-    # Read next line form UART output
-    line = ser.readline().decode('utf-8').strip('\r\n')
-
-    # Print the line so it is available in ctest output.
-    print(line)
-
-    # Check if any test case failed
-    if "\033[31mFAILED\033[0m" in line:
-      suite_passed = False
-
-    # Use regex to check for the summary line to determine if the test suite has completed. TODO:
-    # The summary line always prints PASSED with the number of passed tests (even if it's 0). I feel
-    # it should print FAILED if any test case failed, then we can just look at that summary line to
-    # determine overall pass/fail. For now, we are looking for FAILED in any of the lines to
-    # determine overall suite failure and return status.
-    pattern = r"^\[\s+(\033\[32mPASSED\033\[0m|\033\[31mFAILED\033\[0m)\s+\]\s+(\d+)\s+test\(s\)\.$"
-
-    match = re.match(pattern, line)
-    if match:
-      if suite_passed:
-        sys.exit(0)
-      else:
-        sys.exit(1)
+  test_application = mimxrt1170evk.Core0Application(args.flash_core_0)
+  mimxrt1170evk.ctest.do(test_application=test_application)
 
 
 if __name__ == '__main__':
