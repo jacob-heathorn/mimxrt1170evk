cf173f21215248e701b9ee00faa071feb1d9cc03
Jacob <jacob.heathorn@gmail.com>
2025-03-31 18:32:47 -0500

    unit test for dtcm_bump_allocator

--- test/cm7/ut_dtcm_bump_allocator.cpp
+++ test/cm7/ut_dtcm_bump_allocator.cpp
@@ -1,17 +1,31 @@
 #include "gtest/gtest.h"
 
 #include "utils/bump_allocator.hpp"
+#include <cstdio>
+
+// TODO move to dtcm bump allocator cpp.
+__attribute__((section(".m_dtcm_high")))
+uint8_t dtcm_bump_region[64 * 1024];
 
 TEST(memory, dtcm_bump_allocator) {
-  EXPECT_TRUE(true);
-  EXPECT_FALSE(false);
+  constexpr size_t region_size = 64 * 1024;
+  BumpAllocator dtcm(dtcm_bump_region, region_size);
 
-  EXPECT_TRUE(1);
-  EXPECT_TRUE(1203492);
-  EXPECT_TRUE(-1);
-  EXPECT_TRUE(0.1f);
+  void* p1 = dtcm.alloc(128);
+  void* p2 = dtcm.alloc(64);
+  void* p3 = dtcm.alloc(16, 32);  // explicit alignment
 
-  EXPECT_FALSE(0);
-  EXPECT_FALSE(0.0f);
-  EXPECT_FALSE(-0.0f);
+  printf("Address of p1: %p\r\n", (void*)p1);
+  printf("Address of dtcm: %p\r\n", (void*)dtcm_bump_region);
+  EXPECT_NE(p1, nullptr);
+  EXPECT_EQ(reinterpret_cast<uint32_t>(p1), 0x20040000U);
+  EXPECT_NE(p2, nullptr);
+  EXPECT_NE(p3, nullptr);
+
+  uintptr_t addr3 = reinterpret_cast<uintptr_t>(p3);
+  EXPECT_EQ(addr3 % 32, 0U);  // Ensure 32-byte alignment
+
+  // Allocate too much.
+  void* p_fail = dtcm.alloc(region_size);  // should fail
+  EXPECT_EQ(p_fail, nullptr);
 }
