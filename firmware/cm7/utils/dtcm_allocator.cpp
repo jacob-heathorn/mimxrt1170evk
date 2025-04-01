ad75c8526f9222c0f834058b5edab0d94c34e51a
Jacob <jacob.heathorn@gmail.com>
2025-04-01 13:14:59 -0500

    Delete .cpp allocator

--- firmware/cm7/utils/dtcm_allocator.cpp
+++
@@ -1,6 +0,0 @@
-#include <cstddef>
-#include "utils/dtcm_allocator.hpp"
-
-// // Force the linker to allocate this region.
-// __attribute__((section(".m_dtcm_high")))
-// uint8_t dtcm_bump_region[256 * 1024];
