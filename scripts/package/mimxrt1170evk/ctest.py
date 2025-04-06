6ca6d72e930b3a925b893bf91e5f4288ed3774c4
Jacob <jacob.heathorn@gmail.com>
2025-04-05 19:38:24 -0500

    Look for assert error in ctest

--- scripts/package/mimxrt1170evk/ctest.py
+++ scripts/package/mimxrt1170evk/ctest.py
@@ -8,34 +8,32 @@ SERIAL_DEVICE = os.environ.get("SERIAL_DEVICE")
 
 
 def do(test_application: mimxrt1170evk.Core0Application):
-
   # Create the serial terminal.
   serial_terminal = forge.SerialTerminal(SERIAL_DEVICE, 115200)
 
   # Flash the test application.
   test_application.flash()
 
-  # Analyze test output
+  # Analyze test output.
   suite_passed = True
   while True:
-
-    # Read next line form UART output
+    # Read the next line from UART output.
     line = serial_terminal.readline()
 
-    # Print the line so it is available in ctest output.
+    # Print the line so it is visible in ctest output.
     print(line)
 
-    # Check if any test case failed
+    # If the line contains "ASSERT ERROR", stop the test and report the error.
+    if "ASSERT ERROR" in line:
+      forge.error("ASSERT ERROR encountered: " + line)
+
+    # Check if any test case failed.
     if "\033[31mFAILED\033[0m" in line:
       suite_passed = False
 
-    # Use regex to check for the summary line to determine if the test suite has completed. TODO:
-    # The summary line always prints PASSED with the number of passed tests (even if it's 0). I feel
-    # it should print FAILED if any test case failed, then we can just look at that summary line to
-    # determine overall pass/fail. For now, we are looking for FAILED in any of the lines to
-    # determine overall suite failure and return status.
+    # Use regex to check for the summary line to determine if the test suite has completed.
+    # The summary line always prints the number of passed tests.
     pattern = r"^\[\s+(\033\[32mPASSED\033\[0m|\033\[31mFAILED\033\[0m)\s+\]\s+(\d+)\s+test\(s\)\.$"
-
     match = re.match(pattern, line)
     if match:
       if suite_passed:
