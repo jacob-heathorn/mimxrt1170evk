798bd8566b536fc0af163ba2afbe7728dcd95cee
Jacob <jacob.heathorn@gmail.com>
2025-04-06 11:34:03 -0500

    Hello-threadx, just print first

---
+++ test/cm7/hello_threadx.cpp
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2015, Freescale Semiconductor, Inc.
+ * Copyright 2016-2020 NXP
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include "clock_config.h"
+#include "board.h"
+#include "mcmgr.h"
+#include "registers/codegen/iomuxc.hpp"
+#include "registers/codegen/ccm.hpp"
+#include "core_cm7.h"
+#include "cachel1_armv7.h"
+#include <cstdio>
+
+
+int main(void)
+{
+
+    /* Print the initial banner from Primary core */
+    printf("\r\nHello Threadx\r\n\n");
+}
