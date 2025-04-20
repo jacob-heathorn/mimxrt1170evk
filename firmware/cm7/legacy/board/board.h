02dfd8ab7bb9c1f16806edecca80978d4709ace1
Jacob <jacob.heathorn@gmail.com>
2025-04-19 19:19:43 -0500

    Ping actually fing works

--- firmware/cm7/legacy/board/board.h
+++ firmware/cm7/legacy/board/board.h
@@ -2,6 +2,15 @@
 
 #include "fsl_common.h"
 
+/*! @brief The Ethernet port used by network examples, default use 1G port. */
+/* Below comment is for test script to easily define which port to be used, please don't delete. */
+/* @TEST_ANCHOR */
+#ifndef BOARD_NETWORK_USE_100M_ENET_PORT
+#define BOARD_NETWORK_USE_100M_ENET_PORT (0U)
+#endif
+
+#define BOARD_ENET1_PHY_ADDRESS (0x01U) /* Phy address of enet port 1. */
+
 #if defined(__cplusplus)
 extern "C" {
 #endif /* __cplusplus */
--- test/cm7/hello_netx.cpp
+++ test/cm7/hello_netx.cpp
@@ -1,7 +1,48 @@
+// #define BOARD_NETWORK_USE_100M_ENET_PORT (0U)
+
 #include "fsl_common.h"
 #include "pin_mux.h"
 #include "fsl_gpio.h"
+#include "stdio.h"
+#include "tx_api.h"
+#include "nx_api.h"
 
+/*******************************************************************************
+ * Definitions
+ ******************************************************************************/
+
+/* Define packet pool for the demonstration.  */
+#define NX_PACKET_POOL_SIZE ((1536 + sizeof(NX_PACKET)) * 50)
+
+/*******************************************************************************
+ * Variables
+ ******************************************************************************/
+/* Define the ThreadX and NetX object control blocks...  */
+NX_PACKET_POOL pool_0;
+NX_IP ip_0;
+
+/* Define the IP thread's stack area.  */
+ULONG ip_thread_stack[2 * 1024 / sizeof(ULONG)];
+
+AT_NONCACHEABLE_SECTION_ALIGN(ULONG packet_pool_area[NX_PACKET_POOL_SIZE / 4 + 4], 64);
+
+/* Define the ARP cache area.  */
+ULONG arp_space_area[1024 / sizeof(ULONG)];
+
+/* Define an error counter.  */
+ULONG error_counter;
+
+/*******************************************************************************
+ * Prototypes
+ ******************************************************************************/
+extern "C"
+{
+VOID nx_link_driver(NX_IP_DRIVER *driver_req_ptr);
+}
+
+/*******************************************************************************
+ * Code
+ ******************************************************************************/
 void BOARD_InitModuleClock(void)
 {
     const clock_sys_pll1_config_t sysPll1Config = {
@@ -30,6 +71,15 @@ void IOMUXC_SelectENETClock(void)
     SDK_DelayAtLeastUs(1000, CLOCK_GetFreq(kCLOCK_CpuClk));
 }
 
+extern "C"
+{
+/* return the ENET MDIO interface clock frequency */
+uint32_t BOARD_GetMDIOClock(void)
+{
+    return CLOCK_GetRootClockFreq(kCLOCK_Root_Bus);
+}
+}
+
 int main()
 {
   // Init board hardware.
@@ -46,4 +96,64 @@ int main()
   SDK_DelayAtLeastUs(20000, CLOCK_GetFreq(kCLOCK_CpuClk));
   GPIO_WritePinOutput(GPIO11, 14, 1);
   SDK_DelayAtLeastUs(60000, CLOCK_GetFreq(kCLOCK_CpuClk));
+
+  printf("Entering the kernel..\n\r");
+  tx_kernel_enter();
+  return 0;
+}
+
+/* Define what the initial system looks like.  */
+VOID tx_application_define(void *first_unused_memory)
+{
+    UINT status;
+
+    NX_PARAMETER_NOT_USED(first_unused_memory);
+
+    /* Initialize the NetX system.  */
+    nx_system_initialize();
+
+    /* Create a packet pool.  */
+    status = nx_packet_pool_create(&pool_0, "NetX Main Packet Pool", 1536,
+                                   (ULONG *)(((int)packet_pool_area + 15) & ~15), NX_PACKET_POOL_SIZE);
+
+    /* Check for pool creation error.  */
+    if (status)
+        error_counter++;
+
+    /* Create an IP instance.  */
+    status = nx_ip_create(&ip_0, "NetX IP Instance 0",
+                          IP_ADDRESS(192, 2, 2, 149), 0xFFFFFF00UL,
+                          &pool_0, nx_link_driver, (UCHAR *)ip_thread_stack, sizeof(ip_thread_stack), 1);
+
+    /* Check for IP create errors.  */
+    if (status)
+        error_counter++;
+
+    /* Enable ARP and supply ARP cache memory for IP Instance 0.  */
+    status = nx_arp_enable(&ip_0, (void *)arp_space_area, sizeof(arp_space_area));
+
+    /* Check for ARP enable errors.  */
+    if (status)
+        error_counter++;
+
+    /* Enable TCP traffic.  */
+    status = nx_tcp_enable(&ip_0);
+
+    /* Check for TCP enable errors.  */
+    if (status)
+        error_counter++;
+
+    /* Enable UDP traffic.  */
+    status = nx_udp_enable(&ip_0);
+
+    /* Check for UDP enable errors.  */
+    if (status)
+        error_counter++;
+
+    /* Enable ICMP.  */
+    status = nx_icmp_enable(&ip_0);
+
+    /* Check for errors.  */
+    if (status)
+        error_counter++;
 }
