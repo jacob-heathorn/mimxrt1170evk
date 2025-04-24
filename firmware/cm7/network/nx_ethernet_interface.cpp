3a1b730ef8550185a937c6f52ef42e15de0ca9a1
Jacob <jacob.heathorn@gmail.com>
2025-04-23 20:48:29 -0500

    Allocate packet pool from ocram2

--- firmware/cm7/network/nx_ethernet_interface.cpp
+++ firmware/cm7/network/nx_ethernet_interface.cpp
@@ -18,64 +18,69 @@ VOID nx_link_driver(NX_IP_DRIVER *driver_req_ptr);
 /* Define the IP thread's stack area.  */
 ULONG ip_thread_stack[2 * 1024 / sizeof(ULONG)];
 
-// AT_NONCACHEABLE_SECTION_ALIGN(ULONG packet_pool_area[NX_PACKET_POOL_SIZE / 4 + 4], 64);
-// // ULONG packet_pool_area[NX_PACKET_POOL_SIZE / 4 + 4];
-__attribute__((section(".noncacheable_ocram"), aligned(64)))
-ULONG packet_pool_area[NX_PACKET_POOL_SIZE / 4 + 4];
-
 /* Define the ARP cache area.  */
 ULONG arp_space_area[1024 / sizeof(ULONG)];
 
 NxEthernetInterface::NxEthernetInterface()
 {
   UINT status;
-  ULONG error_counter;
+  ULONG error_counter = 0;
 
   nx_system_initialize();
-  /* Create a packet pool.  */
+  // Create a packet pool.
+  void* packet_pool_area = Ocram2Allocator::instance().allocate(NX_PACKET_POOL_SIZE, 64);
+  assert(packet_pool_area != nullptr);
   status = nx_packet_pool_create(&pool_, "NetX Main Packet Pool", 1536,
     (ULONG *)(((int)packet_pool_area + 15) & ~15), NX_PACKET_POOL_SIZE);
 
-  /* Check for pool creation error.  */
-  if (status)
-  error_counter++;
+  // Check for pool creation error.
+  if (status) {
+    error_counter++;
+  }
 
-  /* Create an IP instance.  */
+  // Create an IP instance.
   status = nx_ip_create(&ip_, "NetX IP Instance 0",
   IP_ADDRESS(192, 2, 2, 149), 0xFFFFFF00UL,
   &pool_, nx_link_driver, (UCHAR *)ip_thread_stack, sizeof(ip_thread_stack), 1);
 
-  /* Check for IP create errors.  */
-  if (status)
-  error_counter++;
+  // Check for IP create errors.
+  if (status) {
+    error_counter++;
+  }
 
-  /* Enable ARP and supply ARP cache memory for IP Instance 0.  */
+  // Enable ARP and supply ARP cache memory for IP Instance 0.
   status = nx_arp_enable(&ip_, (void *)arp_space_area, sizeof(arp_space_area));
 
-  /* Check for ARP enable errors.  */
-  if (status)
-  error_counter++;
+  // Check for ARP enable errors.
+  if (status) {
+    error_counter++;
+  }
 
-  /* Enable TCP traffic.  */
+  // Enable TCP traffic.
   status = nx_tcp_enable(&ip_);
 
-  /* Check for TCP enable errors.  */
-  if (status)
-  error_counter++;
+  // Check for TCP enable errors.
+  if (status) {
+    error_counter++;
+  }
 
-  /* Enable UDP traffic.  */
+  // Enable UDP traffic.
   status = nx_udp_enable(&ip_);
 
-  /* Check for UDP enable errors.  */
-  if (status)
-  error_counter++;
+  // Check for UDP enable errors.
+  if (status) {
+    error_counter++;
+  }
 
-  /* Enable ICMP.  */
+  // Enable ICMP.
   status = nx_icmp_enable(&ip_);
 
-  /* Check for errors.  */
-  if (status)
-  error_counter++;
+  // Check for errors.
+  if (status) {
+    error_counter++;
+  }
+  
+  assert(error_counter == 0);
 }
 
 void NxEthernetInterface::WaitUntilReady()
