ba7b7126272be0a8f125f249b5686f8fa34aa498
Jacob <jacob.heathorn@gmail.com>
2025-05-18 10:03:09 -0700

    deleter.hpp
    * Bump forge to 14db1b7d8f5f996fb40085c9f6b8c736b4bb2269

--- firmware/cm7/network/include/network/ethernet_interface.hpp
+++ firmware/cm7/network/include/network/ethernet_interface.hpp
@@ -2,50 +2,11 @@
 
 #include <memory>
 
+#include "ftl/deleter.hpp"
 #include "network/ipv4.hpp"
 
 class UdpSocket;
 class EthernetInterface;
-
-
-template <typename T>
-class PolymorphicDeleter
-{
-public:
-  PolymorphicDeleter() = default;
-  virtual ~PolymorphicDeleter() = default;
-  
-  // No copying or moving.
-  PolymorphicDeleter(const PolymorphicDeleter&) = delete;            // Delete copy constructor
-  PolymorphicDeleter& operator=(const PolymorphicDeleter&) = delete; // Delete copy assignment operator
-  PolymorphicDeleter(PolymorphicDeleter&&) = delete;                 // Delete move constructor
-  PolymorphicDeleter& operator=(PolymorphicDeleter&&) = delete;      // Delete move assignment operator
-  
-  virtual void operator()(T* s) = 0;
-};
-
-template <typename T>
-class DelegatingDeleter
-{
-public:
-  explicit DelegatingDeleter(PolymorphicDeleter<T> *const polymorphic_deleter)
-    : polymorphic_deleter_{polymorphic_deleter} {}
-  ~DelegatingDeleter() = default;
-
-  // Default copy and move constructors.
-  DelegatingDeleter(const DelegatingDeleter&) = default;
-  DelegatingDeleter(DelegatingDeleter&&) noexcept = default;
-  
-  // Delete assignment operators.
-  DelegatingDeleter& operator=(const DelegatingDeleter&) = delete;  // Delete copy assignment operator
-  DelegatingDeleter& operator=(DelegatingDeleter&&) = delete;       // Delete move assignment operator
-  
-  void operator()(T* s) const {
-    polymorphic_deleter_->operator()(s);
-  }
-private:
-  PolymorphicDeleter<T> *polymorphic_deleter_ = nullptr;
-};
 
 using UdpSocketPtr = std::unique_ptr<UdpSocket, DelegatingDeleter<UdpSocket>>;
 
