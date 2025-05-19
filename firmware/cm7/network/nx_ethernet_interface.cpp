5e80a5ea224549b4df4d15a21390693d861681ac
Jacob <jacob.heathorn@gmail.com>
2025-05-18 20:21:40 -0700

    unique_bump_pool.hpp
    * Bump forge to fa61a1d9f8d51a1e6cbbd50a8569dc5d789d34a3

--- firmware/cm7/network/nx_ethernet_interface.cpp
+++ firmware/cm7/network/nx_ethernet_interface.cpp
@@ -6,46 +6,13 @@
 #include "utils/dtcm_allocator.hpp"
 #include "utils/ocram1_allocator.hpp"
 #include "utils/ocram2_allocator.hpp"
-#include "ftl/bump_pool.hpp"
+#include "ftl/unique_bump_pool.hpp"
 
 extern "C"
 {
 VOID nx_link_driver(NX_IP_DRIVER *driver_req_ptr);
 }
 
-template <typename D, typename B = D>
-class UniqueBumpPool
-  : public BumpPool<D>              // get all the pool logic “for free”
-  , public PolymorphicDeleter<B>    // satisfy the polymorphic-deleter interface
-{
-public:
-  // Inherit the pool’s constructor(s):
-  using BumpPool<D>::BumpPool;
-
-  // We still want no copies or moves:
-  UniqueBumpPool(const UniqueBumpPool&)            = delete;
-  UniqueBumpPool& operator=(const UniqueBumpPool&) = delete;
-  UniqueBumpPool(UniqueBumpPool&&)                 = delete;
-  UniqueBumpPool& operator=(UniqueBumpPool&&)      = delete;
-
-  // PolymorphicDeleter<B> requires us to override operator():
-  void operator()(B* ptr) override
-  {
-    // call the pool’s release, casting back to D*
-    BumpPool<D>::release(static_cast<D*>(ptr));
-  }
-
-  // Acquire returns a unique_ptr that uses *this* as its deleter.
-  template <typename... Args>
-  std::unique_ptr<B, DelegatingDeleter<B>> acquire(Args&&... args)
-  {
-    // Note: unique_ptr will copy/move your deleter (UniqueBumpPool) by value
-    return {
-      BumpPool<D>::acquire(std::forward<Args>(args)...),
-      DelegatingDeleter<B>{this}
-    };
-  }
-};
 
 NxEthernetInterface::NxEthernetInterface(Ipv4Address address, Ipv4Mask mask)
 {
