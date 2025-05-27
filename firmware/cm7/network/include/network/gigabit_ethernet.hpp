43c8b64e244b3b63d9e0e8fcf7cde3b1c279b92c
Jacob <jacob.heathorn@gmail.com>
2025-05-27 10:51:15 -0700

    move more

--- firmware/cm7/network/include/network/gigabit_ethernet.hpp
+++ firmware/cm7/network/include/network/gigabit_ethernet.hpp
@@ -7,7 +7,23 @@ class GigabitEthernet : public NxEthernetInterface, public ftl::Singleton<Gigabi
 {
   friend class ftl::Singleton<GigabitEthernet>;
 private:
-  GigabitEthernet(ftl::ipv4::Address address, ftl::ipv4::Mask mask) : NxEthernetInterface(address, mask) {};
+  GigabitEthernet(ftl::ipv4::Address address, ftl::ipv4::Mask mask) : NxEthernetInterface(address, mask) {
+    // Init board hardware.
+    gpio_pin_config_t gpio_config = {kGPIO_DigitalOutput, 0, kGPIO_NoIntmode};
+
+    this->InitEnetModuleClock();
+    this->IomuxcSelectEnetClock();
+
+    BOARD_InitEnet1GPins();
+    GPIO_PinInit(GPIO11, 14, &gpio_config);
+    /* For a complete PHY reset of RTL8211FDI-CG, this pin must be asserted low for at least 20ms. And
+        * wait for a further 60ms(for internal circuits settling time) before accessing the PHY register */
+    GPIO_WritePinOutput(GPIO11, 14, 0);
+    SDK_DelayAtLeastUs(20000, CLOCK_GetFreq(kCLOCK_CpuClk));
+    GPIO_WritePinOutput(GPIO11, 14, 1);
+    SDK_DelayAtLeastUs(60000, CLOCK_GetFreq(kCLOCK_CpuClk));
+  };
+  
   ~GigabitEthernet() override = default;
 
   // Rule of 5
@@ -23,6 +39,25 @@ public:
 
     // Wait 1 ms for stabilizing clock.
     SDK_DelayAtLeastUs(1000, CLOCK_GetFreq(kCLOCK_CpuClk));
+  }
+
+  void InitEnetModuleClock() {
+    const clock_sys_pll1_config_t sysPll1Config = {
+      .pllDiv2En = true,
+      .pllDiv5En = false,
+      .ss = nullptr,
+      .ssEnable = false
+    };
+    CLOCK_InitSysPll1(&sysPll1Config);
+
+    clock_root_config_t rootCfg = {.clockOff = false, .mux = 4, .div = 4}; /* Generate 125M root clock. */
+    CLOCK_SetRootClock(kCLOCK_Root_Enet2, &rootCfg);
+
+    /* Select syspll2pfd3, 528*18/24 = 396M */
+    CLOCK_InitPfd(kCLOCK_PllSys2, kCLOCK_Pfd3, 24);
+    rootCfg.mux = 7;
+    rootCfg.div = 2;
+    CLOCK_SetRootClock(kCLOCK_Root_Bus, &rootCfg); /* Generate 198M bus clock. */
   }
 
 };
--- test/cm7/hello_netx.cpp
+++ test/cm7/hello_netx.cpp
@@ -13,36 +13,8 @@
 using namespace ftl::ipv4;
 
 
-/*******************************************************************************
- * Definitions
- ******************************************************************************/
-
 #define STACK_SIZE 2048
 uint8_t thread_1_stack[STACK_SIZE];
-
-
-/*******************************************************************************
- * Code
- ******************************************************************************/
-void BOARD_InitModuleClock(void)
-{
-    const clock_sys_pll1_config_t sysPll1Config = {
-        .pllDiv2En = true,
-        .pllDiv5En = false,
-        .ss = nullptr,
-        .ssEnable = false
-    };
-    CLOCK_InitSysPll1(&sysPll1Config);
-
-    clock_root_config_t rootCfg = {.clockOff = false, .mux = 4, .div = 4}; /* Generate 125M root clock. */
-    CLOCK_SetRootClock(kCLOCK_Root_Enet2, &rootCfg);
-
-    /* Select syspll2pfd3, 528*18/24 = 396M */
-    CLOCK_InitPfd(kCLOCK_PllSys2, kCLOCK_Pfd3, 24);
-    rootCfg.mux = 7;
-    rootCfg.div = 2;
-    CLOCK_SetRootClock(kCLOCK_Root_Bus, &rootCfg); /* Generate 198M bus clock. */
-}
 
 
 int main()
@@ -110,22 +82,6 @@ VOID tx_application_define(void *first_unused_memory)
 
     // Set up the etherenet interface
     GigabitEthernet::create("192.2.2.149", Mask{255, 255, 255, 0});
-
-    // Init board hardware.
-    gpio_pin_config_t gpio_config = {kGPIO_DigitalOutput, 0, kGPIO_NoIntmode};
-
-    BOARD_InitModuleClock();
-    GigabitEthernet::instance().IomuxcSelectEnetClock();
-    //   IOMUXC_SelectENETClock();
-
-    BOARD_InitEnet1GPins();
-    GPIO_PinInit(GPIO11, 14, &gpio_config);
-    /* For a complete PHY reset of RTL8211FDI-CG, this pin must be asserted low for at least 20ms. And
-        * wait for a further 60ms(for internal circuits settling time) before accessing the PHY register */
-    GPIO_WritePinOutput(GPIO11, 14, 0);
-    SDK_DelayAtLeastUs(20000, CLOCK_GetFreq(kCLOCK_CpuClk));
-    GPIO_WritePinOutput(GPIO11, 14, 1);
-    SDK_DelayAtLeastUs(60000, CLOCK_GetFreq(kCLOCK_CpuClk));
 
     // Create hello thread.
     static ftl::TxThread thread1(
