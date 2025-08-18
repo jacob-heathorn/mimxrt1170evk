170cfdc7f0f4db5a33314ef20e4c16df36051bbd
Jacob Heathorn <jacob.heathorn@gmail.com>
2025-08-18 02:33:23 -0400

    forbid new/delete

--- firmware/cm4/platform/new_delete.cpp
+++ firmware/cm4/platform/new_delete.cpp
@@ -1,56 +1,63 @@
 #include <cstddef>  // for std::size_t
-#include <cstdlib>  // for malloc and free
 
-extern "C" {
-  void Default_Handler();
+// Error handler for forbidden operations
+[[noreturn]] static void dynamic_alloc_forbidden() {
+    // In debug builds, this will halt the debugger
+    __asm__("bkpt #0");
+    // Infinite loop to ensure we never return
+    while(1) { }
+}
+
+// Custom implementation of operator delete (sized deallocation)
+void operator delete(void* ptr, std::size_t size) noexcept {
+  (void)ptr;
+  (void)size;
+  dynamic_alloc_forbidden();
+}
+
+// Custom implementation of operator delete[] (sized deallocation)
+void operator delete[](void* ptr, std::size_t size) noexcept {
+  (void)ptr;
+  (void)size;
+  dynamic_alloc_forbidden();
 }
 
 // Custom implementation of operator delete
-void operator delete(void* ptr, unsigned int size) {
-  // Your code to free the memory pointed to by 'ptr' with size 'size'
-  // Implement the necessary logic to release the memory in your embedded system.
+void operator delete(void* ptr) noexcept {
   (void)ptr;
-  (void)size;
+  dynamic_alloc_forbidden();
 }
 
 // Custom implementation of operator delete[]
-void operator delete[](void* ptr, unsigned int size) {
-  // Your code to free the memory pointed to by 'ptr' with size 'size'
-  // Implement the necessary logic to release the memory in your embedded system.
+void operator delete[](void* ptr) noexcept {
   (void)ptr;
-  (void)size;
-}
-
-// Custom implementation of operator delete
-void operator delete(void* ptr) {
-  // Your code to free the memory pointed to by 'ptr'
-  // Implement the necessary logic to release the memory in your embedded system.
-  (void)ptr;
-}
-
-// Custom implementation of operator delete[]
-void operator delete[](void* ptr) {
-  // Your code to free the memory pointed to by 'ptr'
-  // Implement the necessary logic to release the memory in your embedded system.
-  (void)ptr;
+  dynamic_alloc_forbidden();
 }
 
 void* operator new(std::size_t size) {
   (void)size;
-  // return malloc(size);
-  // TODO
-  auto x = nullptr;
-  return static_cast<void*>(x);
+  dynamic_alloc_forbidden();
 }
 
 void* operator new[](std::size_t size) {
   (void)size;
-  // TODO
-  // void* p = malloc(size);
-  // if (!p) {
-  //   Default_Handler();
-  // }
-  // return p;
-  auto x = nullptr;
-  return static_cast<void*>(x);
+  dynamic_alloc_forbidden();
+}
+
+// Placement new operators (these are safe - they don't allocate)
+void* operator new(std::size_t, void* ptr) noexcept {
+  return ptr;
+}
+
+void* operator new[](std::size_t, void* ptr) noexcept {
+  return ptr;
+}
+
+// Placement delete operators (required to match placement new)
+void operator delete(void*, void*) noexcept {
+  // Nothing to do - placement delete doesn't free memory
+}
+
+void operator delete[](void*, void*) noexcept {
+  // Nothing to do - placement delete doesn't free memory
 }
