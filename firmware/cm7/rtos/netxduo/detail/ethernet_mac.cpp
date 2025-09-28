82ee95596e5e0aff9fdbd254cb7b76f216b67932
Jacob Heathorn <jacob.heathorn@gmail.com>
2025-09-28 12:55:52 -0400

    own mdio write

--- firmware/cm7/rtos/netxduo/detail/ethernet_mac.cpp
+++ firmware/cm7/rtos/netxduo/detail/ethernet_mac.cpp
@@ -19,9 +19,32 @@ void EthernetMac::mdioInit() {
 }
 
 void EthernetMac::mdioWrite(uint8_t phyAddr, uint8_t regAddr, uint16_t data) {
-    // Use FSL driver to write to PHY register
-    // Return value ignored to match existing behavior
-    (void)ENET_MDIOWrite(EXAMPLE_ENET, phyAddr, regAddr, data);
+    // Direct implementation of MDIO write operation
+
+    // Clear the MDIO access complete event (write 1 to clear)
+    EXAMPLE_ENET->EIR = ENET_EIR_MII_MASK;
+
+    // Start MDIO write command by writing to MMFR register
+    // Format: ST(1) | OP(1 for write) | PA(phyAddr) | RA(regAddr) | TA(2) | DATA(data)
+    EXAMPLE_ENET->MMFR = ENET_MMFR_ST(1U) |           // Start of frame = 01b
+                         ENET_MMFR_OP(1U) |            // Operation = 01b (write)
+                         ENET_MMFR_PA(phyAddr) |       // PHY address
+                         ENET_MMFR_RA(regAddr) |       // Register address
+                         ENET_MMFR_TA(2U) |            // Turnaround = 10b
+                         ENET_MMFR_DATA(data);         // Data to write
+
+    // Wait for MDIO transaction to complete (poll MII interrupt flag)
+    constexpr uint32_t timeout = 100000;  // Timeout counter
+    uint32_t counter = timeout;
+    while (counter > 0) {
+        if (EXAMPLE_ENET->EIR & ENET_EIR_MII_MASK) {
+            break;  // Transaction complete
+        }
+        counter--;
+    }
+
+    // Clear the MDIO access complete event
+    EXAMPLE_ENET->EIR = ENET_EIR_MII_MASK;
 }
 
 uint16_t EthernetMac::mdioRead(uint8_t phyAddr, uint8_t regAddr) {
