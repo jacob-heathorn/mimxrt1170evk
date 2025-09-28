e56a425dc6e32cabe76eac195b86598cdcdb382f
Jacob Heathorn <jacob.heathorn@gmail.com>
2025-09-28 13:15:32 -0400

    full C++ write

--- firmware/cm7/rtos/netxduo/detail/ethernet_mac.cpp
+++ firmware/cm7/rtos/netxduo/detail/ethernet_mac.cpp
@@ -35,13 +35,12 @@ void EthernetMac::mdioInit() {
 }
 
 void EthernetMac::mdioWrite(uint8_t phyAddr, uint8_t regAddr, uint16_t data) {
-    // Direct implementation of MDIO write operation
-
-    // Get reference to MMFR register
+    // Get references to registers
     volatile auto& mmfr = nENET_1G::MMFR::ref();
+    volatile auto& eir = nENET_1G::EIR::ref();
 
     // Clear the MDIO access complete event (write 1 to clear)
-    EXAMPLE_ENET->EIR = ENET_EIR_MII_MASK;
+    eir.bits.MII = 1;
 
     // Reset the MMFR register first
     mmfr.Reset();
@@ -58,14 +57,14 @@ void EthernetMac::mdioWrite(uint8_t phyAddr, uint8_t regAddr, uint16_t data) {
     constexpr uint32_t timeout = 100000;  // Timeout counter
     uint32_t counter = timeout;
     while (counter > 0) {
-        if (EXAMPLE_ENET->EIR & ENET_EIR_MII_MASK) {
+        if (eir.bits.MII) {
             break;  // Transaction complete
         }
         counter--;
     }
 
     // Clear the MDIO access complete event
-    EXAMPLE_ENET->EIR = ENET_EIR_MII_MASK;
+    eir.bits.MII = 1;
 }
 
 uint16_t EthernetMac::mdioRead(uint8_t phyAddr, uint8_t regAddr) {
